00306ae45595b548e997c0a0c6d521636d080859
Abdelmajid Ait Ouaziz <abait-ou@c3r7p5.1337.ma>
2024-03-25 15:38:23 +0000

    Under Construction

--- Minitalk_42_Cursus/ft_client/client.c
+++ Minitalk_42_Cursus/ft_client/client.c
@@ -1,52 +1,62 @@
-#include "../Minitalk.h"
-// SIGUSR1 = 0
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abait-ou <abait-ou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 11:15:21 by abait-ou          #+#    #+#             */
+/*   Updated: 2024/03/13 11:19:04 by abait-ou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 // SIGUSR2 = 1
-void Signal_Sender(pid_t Target_Process_id, char Character_To_Be_Sent)
+// SIGUSR1 = 0
+#include "../Minitalk.h"
+void Signal_Sender(pid_t target_process_id, char character_to_be_sent)
 {
-    int Bits_Sent = 7;
+    int bits_sent = 7;
 
     while (Bits_Sent >= 0)
     {
-        if (((Character_To_Be_Sent >> Bits_Sent) & 1))
+        if (((character_to_be_sent >> bits_sent) & 1))
         {
-            if (kill(Target_Process_id, SIGUSR2) == -1)
+            if (kill(target_process_id, SIGUSR2) == -1)
             {
-                perror("Error sending signal");
                 exit(1);
             }
         }
         else
         {
-            if (kill(Target_Process_id, SIGUSR1) == -1)
+            if (kill(target_process_id, SIGUSR1) == -1)
             {
-                perror("Error sending signal");
                 exit(1);
             }
         }
-        Bits_Sent--;
+        bits_sent--;
         usleep(400);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    pid_t Target_Process_Id;
+    pid_t target_process_id;
     char *message;
-    int Counter = 0;
+    int counter = 0;
 
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <Target_PID> <Message>\n", argv[0]);
+        printf(stderr, "Usage: %s <Target_PID> <Message>\n", argv[0]);
         return 1;
     }
 
-    Target_Process_Id = ft_atoi(argv[1]);
+    target_process_id = ft_atoi(argv[1]);
     message = argv[2];
 
-    while (message[Counter])
+    while (message[counter])
     {
-        Signal_Sender(Target_Process_Id, message[Counter]);
-        Counter++;
+        Signal_Sender(target_process_id, message[counter]);
+        counter++;
     }
 
     usleep(150);
